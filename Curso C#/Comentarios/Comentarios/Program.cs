880d1d9c549538a85a32d6169fbddbd28946c7f7
VictorJon <99852255+VictorJon@users.noreply.github.com>
2022-09-27 19:07:23 -0300

    Add files via upload

---
+++ Curso C#/Comentarios/Comentarios/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Isso é uma string! Aperte uma tecla...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Curso C#/Variaveis/Variaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte num1 = 120;
+
+            Console.WriteLine(num1);
+            Console.ReadKey();
+
+        }
+    }
+}
